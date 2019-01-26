e742750159650ffe4623984e9bc53203bbb4668d
Leslie Alldridge <leslie.alldridge@gmail.com>
2019-01-27 10:23:59 +1300

    finished maps
    finished maps

--- go-guide/maps/main.go
+++ go-guide/maps/main.go
@@ -6,6 +6,22 @@ func main(){
 	colors := map[string]string{
 		"red": "#ff0000",
 		"green": "#4bf745",
+		"white": "ffffff",
 	}
-	fmt.Println(colors)
+
+	// var colors map[string]string
+
+	//Create a map and delete an element inside of it
+
+	// colors := make(map[int]string)
+	// colors[10] = "#ffffff"
+	// delete(colors, 10)
+	
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
