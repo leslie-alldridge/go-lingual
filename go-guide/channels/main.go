86d6d8e38c312775558b1ee52cab86b5674cbb7b
Leslie Alldridge <leslie.alldridge@gmail.com>
2019-01-27 17:37:08 +1300

    routines
    routines

--- go-guide/channels/main.go
+++ go-guide/channels/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"net/http"
 	"fmt"
-	// "os"
+	"time"
 	// "io"
 )
 
@@ -15,17 +15,40 @@ func main()  {
 		"http://trademe.co.nz",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
+	}
+
+	//normal loop
+
+	// for i := 0; i < len(links); i++ {
+	// 	go checkLink(<-c)
+	// }
+
+	//infinite loop
+
+	// for {
+	// 	go checkLink(<-c, c)
+	// }
+
+	for l := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err!= nil {
 		fmt.Println(link, "might be down!")
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- link
 }
